70b1acf6e3a98c0aa8e14c2cc13ba99cc1eecd20
jb3152 <jb3152@student.ohs.school.nz>
2025-07-26 12:41:43 +1200

    i hope that this works

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,87 @@ motor intake(PORT7, false); // Intake motor on port 7 with 18:1 gear ratio
 motor storage(PORT8, false); // Storage motor on port 8 with 18:1 gear ratio
 motor top(PORT9, false); // Top motor on port 9 with 18:1 gear ratio
 motor middle(PORT10, false); // Middle motor on port 10 with 18:1 gear ratio
+motor_group intakeGroup(intake, storage, top, middle); // Group for intake motors
 digital_out scraper(Brain.ThreeWirePort.A);
 controller Controller1 = controller(primary); // Controller for user input
-
+drivetrain Drivetrain(leftdrive, rightdrive); // Drivetrain object using left and right motor groups
+bool RemoteControlCodeEnabled = true; // A flag to control remote control code execution
+bool Controller1LeftShoulderControlMotorsStopped = true;
+bool Controller1RightShoulderControlMotorsStopped = true;
+bool DrivetrainLNeedsToBeStopped_controller1 = true;
+bool DrivetrainRNeedsToBeStopped_controller1 = true;
+bool scrapperUp = true; // Variable to track the state of the scrapper
+int rc_auto_loop_function_controller1(){
+  while(true){
+    if(RemoteControlCodeEnabled){
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
+      if(drivetrainLeftSideSpeed < 7 && drivetrainLeftSideSpeed > -7){
+        if(DrivetrainLNeedsToBeStopped_controller1){
+          leftdrive.stop();
+          DrivetrainLNeedsToBeStopped_controller1 = false;
+        }
+      } else {
+        DrivetrainLNeedsToBeStopped_controller1 = true;
+      }
+      if(drivetrainRightSideSpeed < 7 && drivetrainRightSideSpeed > -7){
+        if(DrivetrainRNeedsToBeStopped_controller1){
+          rightdrive.stop();
+          DrivetrainRNeedsToBeStopped_controller1 = false;
+        }
+      } else {
+        DrivetrainRNeedsToBeStopped_controller1 = true;
+      }
+      if(DrivetrainLNeedsToBeStopped_controller1){
+        leftdrive.setVelocity(drivetrainLeftSideSpeed, percentUnits::pct);
+        leftdrive.spin(directionType::fwd);
+      }
+      if(DrivetrainRNeedsToBeStopped_controller1){
+        rightdrive.setVelocity(drivetrainRightSideSpeed, percentUnits::pct);
+        rightdrive.spin(directionType::fwd);
+      }
+      if(Controller1.ButtonR1.pressing()){
+        intake.spin(fwd);
+        middle.spin(fwd);
+        top.spin(fwd);
+        storage.spin(fwd);
+      } else if(Controller1.ButtonR2.pressing()){
+        intake.spin(reverse);
+        middle.spin(reverse);
+        top.spin(reverse);
+        storage.spin(reverse);
+      } else {
+        intake.stop();
+        middle.stop();
+        top.stop();
+        storage.stop();
+      }
+      if(Controller1.ButtonL1.pressing()){
+        storage.spin(reverse);
+        top.spin(fwd);
+        middle.spin(fwd);
+      }else if(Controller1.ButtonL2.pressing()){
+        storage.spin(reverse);
+        top.spin(reverse);
+        middle.spin(fwd);
+      } else {
+        storage.stop();
+        top.stop();
+        middle.stop();
+        intake.stop();
+      }
+      if(Controller1.ButtonUp.pressing()){
+        if(scrapperUp){
+          scraper.set(true);
+          scrapperUp = false;
+        }else if(Controller1.ButtonDown.pressing()){
+          scraper.set(false);
+          scrapperUp = true;
+        }
+      }
+    }
+  }
+}
 
 
 
@@ -50,8 +128,22 @@ controller Controller1 = controller(primary); // Controller for user input
 void pre_auton(void) {
 
   // All activities that occur before the competition starts
- 
-  
+  scraper.set(true); // Set the scrapper to the up position
+  scrapperUp = true; // Initialize the scrapper state to up
+  // Set the initial velocity of the motors
+  leftdrive.setVelocity(100, percentUnits::pct);
+  rightdrive.setVelocity(100, percentUnits::pct);
+  intake.setVelocity(100, percentUnits::pct);
+  middle.setVelocity(100, percentUnits::pct);
+  top.setVelocity(100, percentUnits::pct);
+  storage.setVelocity(100, percentUnits::pct);
+  // Set the stopping mode of the motors
+  leftdrive.setStopping(brakeType::brake);
+  rightdrive.setStopping(brakeType::brake);
+  intake.setStopping(brakeType::brake);
+  middle.setStopping(brakeType::brake);
+  top.setStopping(brakeType::brake);
+  storage.setStopping(brakeType::brake);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -65,9 +157,22 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+  
+  intakeGroup.spin(fwd); // Start the intake motors
+  Drivetrain.driveFor(25,inches, 90, rpm); // Drive forward at 50% speed
+  wait(1, seconds); // Wait for 1 second
+  Drivetrain.turnFor(turnType::right, 90, degrees); // Turn right 90 degrees
+  Drivetrain.driveFor(25,inches); // Drive forward at 50% speed
+  Drivetrain.turnFor(turnType::left, 90, degrees); // Turn left 90 degrees
+  Drivetrain.driveFor(25,inches); // Drive forward at 50% speed
+  wait(1, seconds); // Wait for 1 second
+  intakeGroup.stop(); // Stop the intake motors
+  top.spin(fwd); // Start the top motor
+  middle.spin(fwd); // Start the middle motor
+  storage.spin(reverse); // Start the storage motor
+  wait(5, seconds);
+  Drivetrain.driveFor(-25,inches); 
+  
 }
 
 /*---------------------------------------------------------------------------*/
